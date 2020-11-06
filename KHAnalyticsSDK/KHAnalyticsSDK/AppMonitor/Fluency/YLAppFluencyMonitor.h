2e7afe602cfe9627276cec0376fd75dc866d6eb8
lcf <objccf@gmail.com>
2020-11-06 18:09:12 +0800

    1.添加卡顿文件

---
+++ KHAnalyticsSDK/KHAnalyticsSDK/AppMonitor/Fluency/YLAppFluencyMonitor.h
@@ -0,0 +1,16 @@
+//
+//  YLAppFluencyMonitor.h
+//  KHAnalyticsSDK
+//
+//  Created by lcf on 2020/11/6.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YLAppFluencyMonitor : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ KHAnalyticsSDK/KHAnalyticsSDK/AppMonitor/Fluency/YLAppFluencyMonitor.m
@@ -0,0 +1,12 @@
+//
+//  YLAppFluencyMonitor.m
+//  KHAnalyticsSDK
+//
+//  Created by lcf on 2020/11/6.
+//
+
+#import "YLAppFluencyMonitor.h"
+
+@implementation YLAppFluencyMonitor
+
+@end
